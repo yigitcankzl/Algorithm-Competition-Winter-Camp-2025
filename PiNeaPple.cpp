b82d2dd5dc2ef6a2db107122f1b45b89ed0b7e45
Yiğitcan Kızıl <kizilyigit33@hotmail.com>
2025-02-07 21:29:36 +0300

    Update PiNeaPple.cpp

--- PiNeaPple.cpp
+++ PiNeaPple.cpp
@@ -1,46 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int checking(const multiset<int, greater<int>>& normal) {
-    int bigone = 0;
-    int count = 0;
-
-    for (auto it = normal.begin(); it != normal.end(); ++it) {
-        count++;
-        if (*it >= count) {
-            bigone = count;
-        } else {
-            break;
-        }
-    }
-    return bigone;
-}
-
 int main() {
     ios::sync_with_stdio(false); 
     cin.tie(nullptr);   
 
     int m, n;
     cin >> m >> n;
-    
-    multiset<int, greater<int>> normal;
-    int element;
+    vector<int> normal(m);
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+
 
     for (int i = 0; i < m; i++) {
-        cin >> element;
-        normal.insert(element);
+        cin >> normal[i];
+        min_heap.push(normal[i]);
+
+        if (min_heap.size() > min_heap.top()) {
+            min_heap.pop();
+        }
+
     }
 
-    int anw = checking(normal);  
+    int h_index = min_heap.size(); 
 
     for (int j = 0; j < n; j++) {
-        cin >> element;
-        
-        normal.insert(element);
-        
-        anw = checking(normal);
-        cout << anw << "\n";
-    }
+        int new_citation;
+        cin >> new_citation;
+        min_heap.push(new_citation);
 
+        if (min_heap.size() > min_heap.top()) {
+            min_heap.pop();
+        }
+
+        h_index = min_heap.size();
+        cout << h_index << endl;
+
+    }
     return 0;
 }
